f042ce5e5a1fcf2d60df8b29cb8825405f7f11d6
Pottabathini Vivekananda <vivekananda.99666@gmail.com>
2026-02-20 15:49:48 +0530

    add gh-pages configuration

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: "/shopsmart", 
     plugins: [react()],
     server: {
         proxy: {
