a88b3d0eaff35ad47e858f2da9f7c643fad91cfa
Pottabathini Vivekananda <vivekananda.99666@gmail.com>
2026-02-20 15:56:10 +0530

    change browserrouter to hashrouter

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Route, HashRouter as Router, Routes } from "react-router-dom";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import About from "./pages/About";
